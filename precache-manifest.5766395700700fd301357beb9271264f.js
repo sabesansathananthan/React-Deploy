fff821cf7c4fadb8feed9421d58a810cdebf4e47
sabesansathananthan <sabesan.16@cse.mrt.ac.lk>
2020-03-12 15:43:22 +0530

    Updates

---
+++ precache-manifest.5766395700700fd301357beb9271264f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b35a65d3d5bad4c7fbc9dc8fda16f898",
+    "url": "/React-Deploy/index.html"
+  },
+  {
+    "revision": "ec96e4a0500dd66517ba",
+    "url": "/React-Deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f1d840840b4be05055eb",
+    "url": "/React-Deploy/static/js/2.d3b1d499.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/React-Deploy/static/js/2.d3b1d499.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec96e4a0500dd66517ba",
+    "url": "/React-Deploy/static/js/main.ef4ae88d.chunk.js"
+  },
+  {
+    "revision": "0d49e3b30d6fe9f5112f",
+    "url": "/React-Deploy/static/js/runtime-main.9759f69c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-Deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
